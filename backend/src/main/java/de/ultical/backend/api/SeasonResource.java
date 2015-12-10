5ce7744af9f1f3c25d56f01475980923759a3417
bas <sebastian.trapp@gmail.com>
2015-12-10 16:07:36 +0100

    Typo in function call fixed

--- backend/src/main/java/de/ultical/backend/api/SeasonResource.java
+++ backend/src/main/java/de/ultical/backend/api/SeasonResource.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
 
@@ -53,7 +60,7 @@ public class SeasonResource {
 		}
 		List<Season> result = null;
 		try {
-			result = this.dataStore.getAllSeaoson();
+			result = this.dataStore.getAllSeasons();
 		} catch (PersistenceException pe) {
 			LOGGER.error("Database access failed!", pe);
 			throw new WebApplicationException("Accessing the database failed", Status.INTERNAL_SERVER_ERROR);
