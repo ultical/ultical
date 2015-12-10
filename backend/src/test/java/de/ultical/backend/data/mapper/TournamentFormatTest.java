d4d1a74f3a2e0c21aae7c05d56e77bb3667df0a6
bas <sebastian.trapp@gmail.com>
2015-12-10 16:00:57 +0100

    Model change reflected in test

--- backend/src/test/java/de/ultical/backend/data/mapper/TournamentFormatTest.java
+++ backend/src/test/java/de/ultical/backend/data/mapper/TournamentFormatTest.java
@@ -1,13 +1,24 @@
 package de.ultical.backend.data.mapper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
 
-import de.ultical.backend.model.*;
+import de.ultical.backend.model.DfvPlayer;
+import de.ultical.backend.model.Gender;
+import de.ultical.backend.model.Season;
+import de.ultical.backend.model.Surface;
+import de.ultical.backend.model.TournamentEditionSingle;
+import de.ultical.backend.model.TournamentFormat;
+import de.ultical.backend.model.User;
 import de.ultical.backend.utils.test.PrepareDBRule;
 
 public class TournamentFormatTest {
@@ -18,113 +29,113 @@ public class TournamentFormatTest {
 
 	@ClassRule
 	public static PrepareDBRule DBRULE = new PrepareDBRule();
-	
+
 	private TournamentFormat format;
 	private TournamentFormatMapper mapper;
 
 	private User admin;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		format = new TournamentFormat();
-		format.setName(NAME);
-		format.setDescription(DESCRIPTION);
-		
+		this.format = new TournamentFormat();
+		this.format.setName(NAME);
+		this.format.setDescription(DESCRIPTION);
+
 		final DfvPlayer player = new DfvPlayer();
 		player.setFirstName("Brodie");
 		player.setLastName("Smith");
 		player.setGender(Gender.MALE);
-		player.setDfvNumber("123456");
+		player.setDfvNumber(123456);
 		player.setBirthDate(LocalDate.of(1979, 1, 25));
 		DBRULE.getSession().getMapper(PlayerMapper.class).insert(player);
 		DBRULE.getSession().getMapper(player.getMapper()).insert(player);
-		
-		admin = new User();
-		admin.setDfvPlayer(player);
-		admin.setEmail("q@q.de");
-		admin.setPassword("secret");
-		DBRULE.getSession().getMapper(admin.getMapper()).insert(admin);
-		
-		mapper = DBRULE.getSession().getMapper(format.getMapper());
+
+		this.admin = new User();
+		this.admin.setDfvPlayer(player);
+		this.admin.setEmail("q@q.de");
+		this.admin.setPassword("secret");
+		DBRULE.getSession().getMapper(this.admin.getMapper()).insert(this.admin);
+
+		this.mapper = DBRULE.getSession().getMapper(this.format.getMapper());
 	}
 
 	@Test
 	public void test() {
-		assertEquals(0,format.getId());
-		mapper.insert(format);
+		assertEquals(0, this.format.getId());
+		this.mapper.insert(this.format);
 		DBRULE.getSession().commit();
-		int formatId = format.getId();
-		
-		TournamentFormat readFormat = mapper.get(formatId);
+		int formatId = this.format.getId();
+
+		TournamentFormat readFormat = this.mapper.get(formatId);
 		assertNotNull(readFormat);
 		assertEquals(1, readFormat.getVersion());
 		assertEquals(NAME, readFormat.getName());
 		assertEquals(DESCRIPTION, readFormat.getDescription());
-		
+
 		this.format.setName("SOTB");
-		int updCount = mapper.update(format);
+		int updCount = this.mapper.update(this.format);
 		assertEquals(0, updCount);
-		
+
 		readFormat.setName("SOTB");
-		updCount = mapper.update(readFormat);
+		updCount = this.mapper.update(readFormat);
 		assertEquals(1, updCount);
 		DBRULE.getSession().commit();
-		
-		readFormat = mapper.get(formatId);
+
+		readFormat = this.mapper.get(formatId);
 		assertEquals("SOTB", readFormat.getName());
 		assertEquals(DESCRIPTION, readFormat.getDescription());
 		assertEquals(2, readFormat.getVersion());
-		
-		mapper.insert(format);
+
+		this.mapper.insert(this.format);
 		DBRULE.getSession().commit();
-		
-		List<TournamentFormat> allFormats = mapper.getAll();
+
+		List<TournamentFormat> allFormats = this.mapper.getAll();
 		assertNotNull(allFormats);
 		assertEquals(2, allFormats.size());
-		
-		mapper.insertAdmin(format, admin);
-		readFormat = mapper.get(format.getId());
+
+		this.mapper.insertAdmin(this.format, this.admin);
+		readFormat = this.mapper.get(this.format.getId());
 		assertNotNull(readFormat);
 		assertNotNull(readFormat.getAdmins());
 		assertFalse(readFormat.getAdmins().isEmpty());
 		assertEquals(1, readFormat.getAdmins().size());
-		
+
 		/*
-		 * create a sample TournamentEdtion to check whether they were set correctly.
+		 * create a sample TournamentEdtion to check whether they were set
+		 * correctly.
 		 */
 		TournamentEditionSingle tes = new TournamentEditionSingle();
 		Season season = new Season();
 		season.setYear(2015);
 		season.setSurface(Surface.TURF);
 		tes.setSeason(season);
-		tes.setTournamentFormat(format);
-		tes.setRegistrationStart(LocalDate.of(2015,10,1));
+		tes.setTournamentFormat(this.format);
+		tes.setRegistrationStart(LocalDate.of(2015, 10, 1));
 		tes.setRegistrationEnd(LocalDate.of(2015, 12, 21));
 		tes.setOrganizerEmail("foo");
 		tes.setOrganizerName("bar");
-		
+
 		/*
-		 * after these three lines, the TournamentFormat format should have two editions.
+		 * after these three lines, the TournamentFormat format should have two
+		 * editions.
 		 */
 		DBRULE.getSession().getMapper(SeasonMapper.class).insert(season);
 		DBRULE.getSession().getMapper(TournamentEditionMapper.class).insert(tes);
 		DBRULE.getSession().getMapper(TournamentEditionMapper.class).insert(tes);
-		
-		TournamentFormat formatWithEditions = mapper.get(format.getId());
+
+		TournamentFormat formatWithEditions = this.mapper.get(this.format.getId());
 		assertNotNull(formatWithEditions);
 		assertNotNull(formatWithEditions.getEditions());
-		assertEquals(2,formatWithEditions.getEditions().size());
-		
-		
-		mapper.delete(format);
+		assertEquals(2, formatWithEditions.getEditions().size());
+
+		this.mapper.delete(this.format);
 		DBRULE.getSession().commit();
-		
-		allFormats = mapper.getAll();
+
+		allFormats = this.mapper.getAll();
 		assertNotNull(allFormats);
 		assertEquals(1, allFormats.size());
-		assertNull(mapper.get(2));
-		
-		
+		assertNull(this.mapper.get(2));
+
 	}
 
 }
