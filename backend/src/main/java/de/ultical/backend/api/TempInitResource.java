764aca3a4dbb7deda8ab6b0aac30653f170cdb45
bas <sebastian.trapp@gmail.com>
2015-12-22 13:17:49 +0100

    Temp commit - mysql insert error testcase

--- backend/src/main/java/de/ultical/backend/api/TempInitResource.java
+++ backend/src/main/java/de/ultical/backend/api/TempInitResource.java
@@ -1,20 +1,14 @@
 package de.ultical.backend.api;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
 
-import de.ultical.backend.api.transferClasses.DfvMvName;
 import de.ultical.backend.app.DfvApiConfig;
 import de.ultical.backend.app.UltiCalConfig;
 import de.ultical.backend.data.DataStore;
+import de.ultical.backend.model.User;
 
 /**
  * Temporary resource to init server
@@ -39,14 +33,47 @@ public class TempInitResource {
 
 	@GET
 	public boolean initRequest() {
-		WebTarget target = this.client.target(this.dfvApi.getUrl()).path("profile").queryParam("token", this.dfvApi.getToken()).queryParam("secret", this.dfvApi.getSecret());
 
-		Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+		/* USER 1 */
+		User bas = new User();
+		bas.setVersion(1);
+		bas.setEmail("bas@knallbude.de");
+		bas.setPassword("password");
+		this.dataStore.addNew(bas);
 
-		List<DfvMvName> response = invocationBuilder.get(new GenericType<List<DfvMvName>>() {
-		});
+		/* USER 2 */
+		User basil = new User();
+		basil.setVersion(2);
+		basil.setEmail("kaffeeee@trinkr.com");
+		basil.setPassword("pw2");
+		this.dataStore.addNew(basil);
 
-		this.dataStore.refreshDfvNames(response);
+		/* DFV MV NAME */
+		// List<DfvMvName> dfvNames = new ArrayList<DfvMvName>();
+		//
+		// DfvMvName dfvName = new DfvMvName();
+		// dfvName.setDfvnr(999);
+		// dfvName.setVorname("Sebastian");
+		// dfvName.setNachname("Trappi");
+		// dfvName.setDse(true);
+		//
+		// dfvNames.add(dfvName);
+		// this.dataStore.refreshDfvNames(dfvNames);
+
+		//
+		// WebTarget target =
+		// this.client.target(this.dfvApi.getUrl()).path("profile").queryParam("token",
+		// this.dfvApi.getToken()).queryParam("secret",
+		// this.dfvApi.getSecret());
+		//
+		// Invocation.Builder invocationBuilder =
+		// target.request(MediaType.APPLICATION_JSON);
+		//
+		// List<DfvMvName> response = invocationBuilder.get(new
+		// GenericType<List<DfvMvName>>() {
+		// });
+		//
+		// this.dataStore.refreshDfvNames(response);
 
 		return true;
 	}
