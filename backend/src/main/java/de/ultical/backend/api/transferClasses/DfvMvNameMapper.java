28dd0bc583ffcd45780a6500cc99594169030e2a
bas <sebastian.trapp@gmail.com>
2015-12-22 12:39:24 +0100

    Added DfvMvNameMapper

---
+++ backend/src/main/java/de/ultical/backend/api/transferClasses/DfvMvNameMapper.java
@@ -0,0 +1,28 @@
+package de.ultical.backend.api.transferClasses;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+public interface DfvMvNameMapper {
+
+	final String SELECT_STMT = "SELECT dfv_number as dfvnr, first_name as vorname, last_name as nachname, dse from DFV_MV_NAME";
+
+	@Select({ SELECT_STMT, "WHERE dfv_number = #{dfvNumber}" })
+	DfvMvName get(int pk);
+
+	@Select(SELECT_STMT)
+	List<DfvMvName> getAll();
+
+	@Insert("INSERT INTO DFV_MV_NAME (dfv_number, first_name, last_name, dse) VALUES (#{dfvnr},#{vorname},#{nachname},#{dse})")
+	@Options(flushCache = true)
+	Integer insert(DfvMvName entity);
+
+	@Delete("DELETE FROM DFV_MV_NAME WHERE 1=1")
+	@Options(flushCache = true)
+	void deleteAll();
+
+}
