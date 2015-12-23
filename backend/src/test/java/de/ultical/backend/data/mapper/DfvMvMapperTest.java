9408eb89352a8a207ae5f94f8ebae9c564e64709
Basil <kaffeetrinker+github@gmail.com>
2015-12-23 20:43:24 +0100

    tested for primarykeyconstraint violations

--- backend/src/test/java/de/ultical/backend/data/mapper/DfvMvMapperTest.java
+++ backend/src/test/java/de/ultical/backend/data/mapper/DfvMvMapperTest.java
@@ -1,9 +1,12 @@
 package de.ultical.backend.data.mapper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.junit.*;
 
 import de.ultical.backend.api.transferClasses.DfvMvName;
@@ -17,10 +20,10 @@ public class DfvMvMapperTest {
 
 	@ClassRule
 	public static PrepareDBRule DBRULE = new PrepareDBRule();
-	
+
 	private DfvMvName dfvMvName;
 	private DfvMvNameMapper mapper;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		dfvMvName = new DfvMvName();
@@ -28,7 +31,7 @@ public class DfvMvMapperTest {
 		dfvMvName.setVorname(VORNAME);
 		dfvMvName.setNachname(NACHNAME);
 		dfvMvName.setDse(true);
-		
+
 		mapper = DBRULE.getSession().getMapper(DfvMvNameMapper.class);
 	}
 
@@ -41,27 +44,39 @@ public class DfvMvMapperTest {
 		assertEquals(VORNAME, found.getVorname());
 		assertEquals(NACHNAME, found.getNachname());
 		assertEquals(true, found.isDse());
-		
+
 		DfvMvName other = new DfvMvName();
 		other.setDfvnr(1234568);
 		other.setVorname(VORNAME);
 		other.setNachname(NACHNAME);
 		mapper.insert(other);
-		
+
 		other = new DfvMvName();
 		other.setDfvnr(1234569);
 		other.setVorname(VORNAME);
 		other.setNachname(NACHNAME);
 		mapper.insert(other);
-		
+
 		List<DfvMvName> names = mapper.getAll();
 		assertNotNull(names);
 		assertEquals(3, names.size());
-		
+
 		mapper.deleteAll();
 		assertNull(mapper.get(1234567));
 		assertNull(mapper.get(1234568));
 		assertNull(mapper.get(1234569));
+	}
+
+	@Test(expected = PersistenceException.class)
+	public void testPrimaryKeyConstraintViolation() throws Exception {
+		try {
+			this.mapper.insert(dfvMvName);
+			this.mapper.insert(dfvMvName);
+		} finally {
+			// avoids an exception in case this test is run before the test()
+			// method.
+			this.mapper.deleteAll();
+		}
 	}
 
 }
