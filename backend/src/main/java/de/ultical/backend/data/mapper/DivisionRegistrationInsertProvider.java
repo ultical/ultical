edec384df2e401d23603be80e5915545fec3d57b
bas <sebastian.trapp@gmail.com>
2016-01-26 12:32:13 +0100

    Introducing division_identifier to DivisionRegistration Table to enable multiple divisions of the same type and age in one tournament (e.g. 'pro division', 'beginners division', ...)

--- backend/src/main/java/de/ultical/backend/data/mapper/DivisionRegistrationInsertProvider.java
+++ backend/src/main/java/de/ultical/backend/data/mapper/DivisionRegistrationInsertProvider.java
@@ -5,23 +5,26 @@ import java.util.Objects;
 
 import org.apache.ibatis.jdbc.SQL;
 
-import de.ultical.backend.model.*;
+import de.ultical.backend.model.DivisionRegistration;
+import de.ultical.backend.model.DivisionRegistrationPlayers;
+import de.ultical.backend.model.TournamentEdition;
 
 public class DivisionRegistrationInsertProvider {
 
-	public static String getInsertSql(final Map<String, Object> params) {
-		DivisionRegistration reg = (DivisionRegistration) Objects.requireNonNull(params.get("reg"));
-		TournamentEdition edition = (TournamentEdition) Objects.requireNonNull(params.get("edition"));
-		Objects.requireNonNull(edition);
-		final boolean is_player_registration = reg instanceof DivisionRegistrationPlayers;
-		SQL sql = new SQL();
-		sql.INSERT_INTO("DIVISION_REGISTRATION");
-		sql.VALUES("tournament_edition", String.valueOf(edition.getId()));
-		sql.VALUES("division_type", "'"+reg.getDivisionType().name()+"'");
-		sql.VALUES("division_age", "'"+reg.getDivisionAge().name()+"'");
-		sql.VALUES("number_of_spots", String.valueOf(reg.getNumberSpots()));
-		sql.VALUES("is_player_registration", Boolean.toString(is_player_registration));
-		
-		return sql.toString();
-	}
+    public static String getInsertSql(final Map<String, Object> params) {
+        DivisionRegistration reg = (DivisionRegistration) Objects.requireNonNull(params.get("reg"));
+        TournamentEdition edition = (TournamentEdition) Objects.requireNonNull(params.get("edition"));
+        Objects.requireNonNull(edition);
+        final boolean is_player_registration = reg instanceof DivisionRegistrationPlayers;
+        SQL sql = new SQL();
+        sql.INSERT_INTO("DIVISION_REGISTRATION");
+        sql.VALUES("tournament_edition", String.valueOf(edition.getId()));
+        sql.VALUES("division_type", "'" + reg.getDivisionType().name() + "'");
+        sql.VALUES("division_age", "'" + reg.getDivisionAge().name() + "'");
+        sql.VALUES("number_of_spots", String.valueOf(reg.getNumberSpots()));
+        sql.VALUES("is_player_registration", Boolean.toString(is_player_registration));
+        sql.VALUES("division_identifier", "'" + reg.getDivisionIdentifier() + "'");
+
+        return sql.toString();
+    }
 }
