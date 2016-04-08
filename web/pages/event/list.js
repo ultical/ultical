cde6ab934e4255bac5bfd683efff020a190a95d2
bas <sebastian.trapp@gmail.com>
2016-04-09 00:49:40 +0200

    Collapse months before the current one in calendar view

--- web/pages/event/list.js
+++ web/pages/event/list.js
@@ -2,8 +2,8 @@
 
 angular.module('ultical.events', [])
 
-.controller('EventListCtrl', ['$scope', 'storage', '$state', '$filter', 'Slug', 'headService',
-                              function($scope, storage, $state, $filter, slug, headService) {
+.controller('EventListCtrl', ['$scope', 'storage', '$state', '$filter', 'Slug', 'headService', 'moment',
+                              function($scope, storage, $state, $filter, slug, headService, moment) {
 
   headService.setTitle('event.list.title', {});
 
@@ -18,5 +18,31 @@ angular.module('ultical.events', [])
 		$scope.events = data;
 	});
 
+  $scope.show = {
+    beginningOfYear: false,
+    year: {},
+  };
+
+  $scope.current = {
+    january: moment().month(0),
+    now: moment(),
+    nextYear: moment().add(1, 'years').month(0).day(0),
+    lastMonth: moment().subtract(1, 'months'),
+    day: moment().subtract(3, 'days'),
+  };
+
+  $scope.eventFilter = function(event, index, array) {
+    var eventStartDate = moment(event.startDate);
+    if (eventStartDate.isAfter($scope.current.day) && eventStartDate.isBefore($scope.current.nextYear)) {
+        return true;
+    }
+    if (eventStartDate.isSame($scope.current.now, 'year') && $scope.show.beginningOfYear) {
+      return true;
+    }
+    if (eventStartDate.format('YYYY') in $scope.show.year) {
+      return true;
+    }
+    return false;
+  };
 
 }]);
