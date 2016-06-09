72489bc07e68ac4ba02dec6e70780b75d9dbdafb
Dr. Basil Becker <basil.becker@nanologika.de>
2016-06-09 21:51:13 +0200

    completed Simulator and added test-cases
    * Simulator now returns verbaende, vereine and profiles
    * added Registration Test-cases for the following scenarios:
        * successfull registration
        * registration request with an unknown name
        * registration request with a unsatisfying password
        * ambiguous registration request that could not be resolved.
    
    Still open is the complete integration into maven. Mainly a way to drop
    all existing tables prior to liquibase update is missing.

--- backend/src/test/java/de/ultical/backend/app/RegistrationIntegrationTest.java
+++ backend/src/test/java/de/ultical/backend/app/RegistrationIntegrationTest.java
@@ -1,5 +1,15 @@
 package de.ultical.backend.app;
 
+import static com.jayway.restassured.RestAssured.given;
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.mail.Address;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.ws.rs.core.MediaType;
 
 import org.hamcrest.Matchers;
@@ -8,14 +18,16 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.ServerSetup;
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
 
 import de.ultical.backend.utils.test.DfvMvSimulator;
 import io.dropwizard.configuration.ConfigurationFactory;
 import io.dropwizard.configuration.FileConfigurationSourceProvider;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.testing.DropwizardTestSupport;
-import io.dropwizard.testing.FixtureHelpers;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardClientRule;
 
@@ -24,31 +36,63 @@ public class RegistrationIntegrationTest {
 	public static final String REGISTER_PATH = "/command/register";
 	@ClassRule
 	public static DropwizardClientRule dcr = new DropwizardClientRule(new DfvMvSimulator());
-	
+
+	@ClassRule
+	public static GreenMailRule greenMail = new GreenMailRule(new ServerSetup(5587, "localhost", "smtp"));
+
 	public static DropwizardTestSupport<UltiCalConfig> testSupport;
-	
+
 	@BeforeClass
 	public static void beforeClass() throws Exception {
-		ConfigurationFactory<UltiCalConfig> configFactory = new ConfigurationFactory<UltiCalConfig>(UltiCalConfig.class, null, Jackson.newObjectMapper(), "dw");
-		UltiCalConfig config = configFactory.build(new FileConfigurationSourceProvider(),ResourceHelpers.resourceFilePath("testConfig.yaml"));
+		ConfigurationFactory<UltiCalConfig> configFactory = new ConfigurationFactory<UltiCalConfig>(UltiCalConfig.class,
+				null, Jackson.newObjectMapper(), "dw");
+		UltiCalConfig config = configFactory.build(new FileConfigurationSourceProvider(),
+				ResourceHelpers.resourceFilePath("testConfig.yaml"));
 		config.getDfvApi().setUrl(dcr.baseUri().toString());
 		testSupport = new DropwizardTestSupport<UltiCalConfig>(Application.class, config);
-		
+
 		testSupport.before();
+
+		RestAssured.port = testSupport.getLocalPort();
 	}
-	
+
 	@AfterClass
 	public static void after() {
 		testSupport.after();
 	}
 
 	@Test
-	public void test() {
-		RestAssured.port = testSupport.getLocalPort();
-		RestAssured.given().contentType(MediaType.APPLICATION_JSON)
-				.body(FixtureHelpers.fixture("fixtures/registrationTest/shortPassword.json")).post(REGISTER_PATH).then()
-				.statusCode(200).and().contentType(MediaType.APPLICATION_JSON).and()
-				.body("status", Matchers.equalTo("VALIDATION_ERROR"));
+	public void testPasswordTooShort() {
+		given().contentType(MediaType.APPLICATION_JSON).body(fixture("fixtures/registrationTest/shortPassword.json"))
+				.accept(ContentType.JSON).post(REGISTER_PATH).then().statusCode(200).and()
+				.contentType(MediaType.APPLICATION_JSON).and().body("status", Matchers.equalTo("VALIDATION_ERROR"));
 	}
 
+	@Test
+	public void testUnknownName() {
+		given().contentType(MediaType.APPLICATION_JSON).body(fixture("fixtures/registrationTest/unknownName.json"))
+				.accept(ContentType.JSON).post(REGISTER_PATH).then().statusCode(200).and().contentType(ContentType.JSON)
+				.and().body("status", Matchers.equalTo("NOT_FOUND"));
+	}
+
+	@Test
+	public void testSuccessfulRegistration_unambiguous() throws Exception {
+		given().contentType(ContentType.JSON).accept(ContentType.JSON)
+				.body(fixture("fixtures/registrationTest/success_unambiguous.json")).post(REGISTER_PATH).then()
+				.statusCode(200).and().body("status", Matchers.equalTo("SUCCESS")).and().contentType(ContentType.JSON);
+		assertTrue(greenMail.waitForIncomingEmail(1));
+		final MimeMessage message = greenMail.getReceivedMessages()[0];
+		Address[] recipients = message.getRecipients(RecipientType.TO);
+		assertNotNull(recipients);
+		assertEquals(1, recipients.length);
+		assertEquals("test@ultical.de", ((InternetAddress) recipients[0]).getAddress());
+	}
+
+	@Test
+	public void testAmbiguousRequest() {
+		given().contentType(ContentType.JSON).accept(ContentType.JSON)
+				.body(fixture("fixtures/registrationTest/identicalNameAndBirthdate.json")).post(REGISTER_PATH).then()
+				.statusCode(200).and().contentType(ContentType.JSON).and().body("status", Matchers.equalTo("AMBIGUOUS"))
+				.and().body("clubs", Matchers.hasSize(2));
+	}
 }
--- backend/src/test/java/de/ultical/backend/utils/test/DfvMvSimulator.java
+++ backend/src/test/java/de/ultical/backend/utils/test/DfvMvSimulator.java
@@ -2,6 +2,7 @@ package de.ultical.backend.utils.test;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -12,12 +13,24 @@ import io.dropwizard.testing.FixtureHelpers;
 @Path("/")
 public class DfvMvSimulator {
 
-	@Path("profiles")
+	@Path("profile")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllProfiles(@QueryParam("secret") String secret, @QueryParam("token") String token) {
-		return Response.ok().type(MediaType.APPLICATION_JSON).entity(FixtureHelpers.fixture("allProfiles.json"))
+		return Response.ok().type(MediaType.APPLICATION_JSON).entity(FixtureHelpers.fixture("fixtures/allProfiles.json"))
 				.build();
+	}
+	
+	@GET
+	@Path("profil/{dfvnr}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getSingleProfile(@QueryParam("secret") String secret, @QueryParam("token") String token, @PathParam("dfvnr") Integer dfvNr) {
+		try {
+			final String fixture = FixtureHelpers.fixture("fixtures/profile-"+dfvNr+".json");
+			return Response.ok().type(MediaType.APPLICATION_JSON).entity(fixture).build();
+		} catch (IllegalArgumentException iae) {
+			return Response.status(404).build();
+		}
 	}
 	
 	@Path("verbaende")
@@ -25,5 +38,12 @@ public class DfvMvSimulator {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getVerbaende(@QueryParam("secret") String secret, @QueryParam("token") String token) {
 		return Response.ok().type(MediaType.APPLICATION_JSON_TYPE).entity("[{ \"name\":\"Deutscher Frisbeesport Verband\",\"acronym\":\"DFV\",\"id\":1,\"version\":1}]").build();
+	}
+	
+	@GET
+	@Path("vereine")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getVereine(@QueryParam("secret") String secret, @QueryParam("token") String token) {
+		return Response.ok().type(MediaType.APPLICATION_JSON).entity("[{\"name\":\"Test-Verein 1 e.V.\",\"vereinsnr\":1,\"verband\":1},{\"name\":\"Zweiter Testverein\",\"vereinsnr\":2,\"verband\":1}]").build();
 	}
 }
