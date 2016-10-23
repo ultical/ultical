8402b98d245cf9f1a56968364aa1173a2c6ae77c
Basil <basil@ultical.com>
2016-10-23 23:34:52 +0200

    added invocation of IBANValidationService into DivisionResource and added proper unit-tests for the resource

--- backend/src/main/java/de/ultical/backend/api/DivisionResource.java
+++ backend/src/main/java/de/ultical/backend/api/DivisionResource.java
@@ -18,11 +18,14 @@ import org.slf4j.LoggerFactory;
 import de.ultical.backend.app.Authenticator;
 import de.ultical.backend.data.DataStore;
 import de.ultical.backend.data.DataStore.DataStoreCloseable;
+import de.ultical.backend.exception.IBANValidationException;
 import de.ultical.backend.model.DivisionRegistrationTeams;
 import de.ultical.backend.model.Roster;
 import de.ultical.backend.model.TeamRegistration;
 import de.ultical.backend.model.User;
+import de.ultical.backend.services.IBANValidationService;
 import io.dropwizard.auth.Auth;
+import java.util.Objects;
 
 @Path("/divisions")
 public class DivisionResource {
@@ -31,10 +34,16 @@ public class DivisionResource {
 
     private static final String DB_ACCESS_FAILURE = "Accessing the database failed!";
 
-    private final static Logger LOG = LoggerFactory.getLogger(DivisionResource.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DivisionResource.class);
 
+    private final DataStore dStore;
+    private final IBANValidationService ibanService;
+    
     @Inject
-    DataStore dStore;
+    public DivisionResource(final DataStore dataStore, final IBANValidationService ibanSvc) {
+	this.dStore = Objects.requireNonNull(dataStore);
+	this.ibanService = Objects.requireNonNull(ibanSvc);
+    }
 
     @POST
     @Path("/{divisionId}/registerTeam/{rosterId}")
@@ -45,23 +54,11 @@ public class DivisionResource {
             throw new WebApplicationException(INJECTION_FAILURE);
         }
 
-        /*
-         * If the client provided a team-instance as part of the payload, then
-         * we expect that the provided team's id and the request url's teamId
-         * are equals. If no team is provided, we create a fake team object that
-         * only serves as holder for the id.
-         */
-        if (teamReg.getRoster() == null) {
-            final Roster fakeRoster = new Roster();
-            fakeRoster.setId(rosterId);
-            teamReg.setRoster(fakeRoster);
-        } else {
-            final Roster receivedRoster = teamReg.getRoster();
-            if (!rosterId.equals(receivedRoster.getId())) {
-                throw new WebApplicationException("Request URL and payload do not match! team-id differs!",
-                        Status.NOT_ACCEPTABLE);
-            }
-        }
+	try {
+	    this.validateTeamReg(teamReg, rosterId);
+	} catch (IBANValidationException ive) {
+	    throw new WebApplicationException("Provided IBAN is invalid",ive,Status.BAD_REQUEST);
+	}
 
         try (DataStoreCloseable c = this.dStore.getClosable()) {
             Roster loadedRoster = this.dStore.get(rosterId, Roster.class);
@@ -78,6 +75,35 @@ public class DivisionResource {
         }
     }
 
+    private void validateTeamReg(final TeamRegistration teamReg, final Integer rosterId) {
+	/*
+         * If the client provided a team-instance as part of the
+         * payload, then we expect that the provided team's id and the
+         * request url's teamId are equals. If no team is provided, we
+         * create a fake team object that only serves as holder for
+         * the id.
+         */
+        if (teamReg.getRoster() == null) {
+            final Roster fakeRoster = new Roster();
+            fakeRoster.setId(rosterId);
+            teamReg.setRoster(fakeRoster);
+        } else {
+            final Roster receivedRoster = teamReg.getRoster();
+            if (!rosterId.equals(receivedRoster.getId())) {
+                throw new WebApplicationException("Request URL and payload do not match! team-id differs!",
+                        Status.NOT_ACCEPTABLE);
+            }
+        }
+
+	if (teamReg.getIban() != null) {
+	    final String receivedIban = teamReg.getIban();
+	    teamReg.setIban(receivedIban.toUpperCase());
+	    this.ibanService.validateIBAN(teamReg.getIban());
+	} else {
+	    throw new WebApplicationException("IBAN must be provided", Status.BAD_REQUEST);
+	}
+    }
+    
     @DELETE
     @Path("/{divisionId}/registerTeam/{rosterId}")
     public void unregisterTeam(@Auth @NotNull User currentUser, @PathParam("divisionId") Integer divId,
