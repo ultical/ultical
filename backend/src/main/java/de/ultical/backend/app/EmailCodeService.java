0f9d96c2eb6ba131366d91f2a7eafa60a213fb53
Steffen Mecke <stm2@users.sf.net>
2025-04-16 13:02:23 +0200

    fixed broken links for forgotten password links

--- backend/src/main/java/de/ultical/backend/app/EmailCodeService.java
+++ backend/src/main/java/de/ultical/backend/app/EmailCodeService.java
@@ -1,6 +1,7 @@
 package de.ultical.backend.app;
 
 import java.math.BigInteger;
+import java.net.URISyntaxException;
 import java.security.SecureRandom;
 import java.util.HashMap;
 
@@ -30,12 +31,16 @@ public class EmailCodeService {
         MailCode mailCode = this.createMailCode(user, MailCodeType.FORGOT_PASSWORD);
 
         if (this.dataStore.saveMailCode(mailCode)) {
-            HashMap<String, String> values = new HashMap<String, String>();
-            values.put("link", this.createForgotPasswordLink(mailCode));
-            SystemMessage message = new ForgotPasswordMessage(values);
-            message.addRecipient(user.getEmail(), user.getDfvPlayer().getFirstName(), user.getFullName());
+            try {
+                HashMap<String, String> values = new HashMap<String, String>();
+                values.put("link", this.createForgotPasswordLink(mailCode));
+                SystemMessage message = new ForgotPasswordMessage(values);
+                message.addRecipient(user.getEmail(), user.getDfvPlayer().getFirstName(), user.getFullName());
 
-            mailClient.sendMail(message);
+                mailClient.sendMail(message);
+            } catch (URISyntaxException e) {
+                return false;
+            }
             return true;
         }
         return false;
@@ -46,16 +51,20 @@ public class EmailCodeService {
         MailCode mailCode = this.createMailCode(user, MailCodeType.CONFIRM_EMAIL);
 
         if (this.dataStore.saveMailCode(mailCode)) {
-            SystemMessage message = new ConfirmEmailMessage();
-            message.addRecipient(user.getEmail(), user.getDfvPlayer().getFirstName(), user.getFullName());
+            try {
+                SystemMessage message = new ConfirmEmailMessage();
+                message.addRecipient(user.getEmail(), user.getDfvPlayer().getFirstName(), user.getFullName());
 
-            HashMap<String, String> values = new HashMap<String, String>();
-            values.put("pageName", "DFV-Turniere");
-            values.put("link", this.createEmailConfirmationLink(mailCode));
-            values.put("needsDfvOptIn", user.isDfvEmailOptIn() ? "0" : "1");
-            message.init(values);
-            mailClient.sendMail(message);
-            return true;
+                HashMap<String, String> values = new HashMap<String, String>();
+                values.put("pageName", "DFV-Turniere");
+                values.put("link", this.createEmailConfirmationLink(mailCode));
+                values.put("needsDfvOptIn", user.isDfvEmailOptIn() ? "0" : "1");
+                message.init(values);
+                mailClient.sendMail(message);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
         }
         return false;
     }
@@ -64,31 +73,51 @@ public class EmailCodeService {
         MailCode mailCode = this.createMailCode(user, MailCodeType.DFV_MAIL_OPT_IN);
 
         if (this.dataStore.saveMailCode(mailCode)) {
-            SystemMessage optInMessage = new DfvOptInMessage();
-            optInMessage.addRecipient(recipientEmail, user.getDfvPlayer().getFirstName(), user.getFullName());
+            try {
+                SystemMessage optInMessage = new DfvOptInMessage();
+                optInMessage.addRecipient(recipientEmail, user.getDfvPlayer().getFirstName(), user.getFullName());
 
-            HashMap<String, String> optInValues = new HashMap<String, String>();
-            optInValues.put("pageName", "DFV-Turniere");
-            optInValues.put("link", this.createEmailConfirmationLink(mailCode));
-            optInValues.put("registeredEmail", user.getEmail());
-            optInMessage.init(optInValues);
+                HashMap<String, String> optInValues = new HashMap<String, String>();
+                optInValues.put("pageName", "DFV-Turniere");
+                optInValues.put("link", this.createEmailConfirmationLink(mailCode));
+                optInValues.put("registeredEmail", user.getEmail());
+                optInMessage.init(optInValues);
 
-            mailClient.sendMail(optInMessage);
-            return true;
+                mailClient.sendMail(optInMessage);
+                return true;
+            } catch (URISyntaxException e) {
+                return false;
+            }
         }
         return false;
     }
 
-    private String createEmailConfirmationLink(MailCode mailCode) {
+    private String createEmailConfirmationLink(MailCode mailCode) throws URISyntaxException {
         // TODO hardcoded language - we could use a preffered language property
         // in the user entity
-        return this.websiteUrl + "de/confirm/" + mailCode.getCode();
+        return buildUrl(websiteUrl, "de/confirm/" + mailCode.getCode());
     }
 
-    private String createForgotPasswordLink(MailCode mailCode) {
+    private String createForgotPasswordLink(MailCode mailCode) throws URISyntaxException {
         // TODO hardcoded language - we could use a preffered language property
         // in the user entity
-        return this.websiteUrl + "de/forgot/password/" + mailCode.getCode();
+        return buildUrl(this.websiteUrl, "de/forgot/password/" + mailCode.getCode());
+    }
+
+    protected String buildUrl(String baseUrl, String path) throws URISyntaxException {
+        if (baseUrl == null || baseUrl.isEmpty()) {
+            throw new URISyntaxException(baseUrl, "Base URL is empty");
+        }
+        if (path == null || path.isEmpty()) {
+            return baseUrl;
+        }
+        if (!baseUrl.endsWith("/")) {
+            baseUrl += "/";
+        }
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        return baseUrl + path;
     }
 
     public MailCode createMailCode(User user, MailCodeType type) {
